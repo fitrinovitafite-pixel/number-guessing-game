3b50648ebc74d72e83ebf391678123f79d43bf20
fitrinovitafite-pixel <fitrinovitafite@gmail.com>
2026-07-29 10:03:25 +0700

    add Number Guessing Game project

---
+++ number_guessing_game.py
@@ -0,0 +1,32 @@
+#mengimport modul random
+import random
+
+print ("==== NUMBER GUESSING GAME ====")
+
+#komputer memilih angka acak
+angka_rahasia = random.randint(1, 100)
+#print (f"(DEBUG) angka rahasia komputer : {angka_rahasia}")
+
+percobaan = 0
+#pengulangan sampai tebakan benar
+while True:
+    tebakan = int(input("masukan tebakan (1-100): "))
+
+    #validasi input
+    if tebakan < 1 or tebakan > 100:
+        print ("masukan angka antara 1 sampai 100")
+        continue
+
+    #menambah jumlah percobaan tebakan
+    percobaan += 1
+
+    #mengecek tebakan
+    if tebakan < angka_rahasia:
+        print ("tebakan salah. \nAngka terlalu kecil.")
+    elif tebakan > angka_rahasia:
+        print ("tebakan salah. \nAngka terlalu besar.")
+    else:
+        print ("selamat! tebakan benar ")
+        print (f"kamu berhasil dalam {percobaan} percobaan. ")
+        break
+    
